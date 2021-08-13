9194c0db63f985420b7e851423365dc9b4ad2ea0
catalin.mema@titeica.com <catalin.mema@titeica.com>
2021-08-13 15:20:08 +0300

    feat: code

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mangoose = require("mongoose");
 const app = express();
 const port = 5000;
-var objectId = require('mongodb').ObjectID;
+// var objectId = require('mongodb').ObjectID;
 app.use(express.json())
 
   const connectDb = async() => {
@@ -33,10 +33,15 @@ const TodoSchema = new mangoose.Schema({
     date_todo:{
         type: Date,
         required:true,
+    },
+    completed_todo:{
+        type: Boolean,
+        required:true,
     }
 })
 
 const Todo = mangoose.model('todo',TodoSchema);
+
 
 app.get('/getTodos', async (req,res)=>{
     Todo.find().then(todo => res.json(todo));
@@ -44,7 +49,7 @@ app.get('/getTodos', async (req,res)=>{
 })
 app.get('/getTodos/page/:nr_skips', async (req,res)=>{
     const skipsElements=req.params.nr_skips;
-    Todo.find().skip(Number(skipsElements)).limit(8).sort({_id:-1}).then(recipe => res.json(recipe))
+    Todo.find().skip(Number(skipsElements)).limit(8).sort({date_todo:1}).then(todo => res.json(todo))
 })
 app.get('/search/:text_value', async (req,res)=>{
     const text_todo=req.params.text_value;
@@ -67,7 +72,8 @@ app.get('/dayfilter/:day/:nr_skips', async (req,res)=>{
 app.post('/todos',async(req,res) =>{
     const newTodo = new Todo({
         todo_text:req.body.todo_text,
-        date_todo:req.body.date_todo
+        date_todo:req.body.date_todo,
+        completed_todo:req.body.completed_todo,
     });
     try {
         await newTodo.save();
@@ -89,12 +95,24 @@ app.post('/delete',async (req,res) =>{
 app.post('/update', async (req,res) =>{
     var id = req.body.id;
     try {
-        await Todo.updateOne({_id: objectId(id)},{todo_text: req.body.todo_text},{date_todo: req.body.date_todo})
+        await Todo.updateOne({_id: id},{todo_text: req.body.todo_text})
         res.send("Todo updated");
       } catch {
         res.status(404).send({ error: "Todo is not found!" });
       }
 })
+
+
+app.post('/completed', async (req,res) =>{
+    var id = req.body.id;
+    let cmp = req.body.completed_todo;
+    try {
+        await Todo.updateOne({_id: id},{completed_todo:cmp}).then((result)=>res.send("To do complete"))
+      } catch {
+        res.status(404).send({ error: "Todo is not found!" });
+      }
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
